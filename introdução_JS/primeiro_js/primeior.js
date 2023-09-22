20bb9399b70f19beedfe007d922cc279b822812e
Marcos Vinicius <contato@marcosviniciusdev.com>
2023-09-22 14:08:46 -0300

    Explicação lecionada em sala de aula sobre JS.
    Signed-off-by: Marcos Vinicius <contato@marcosviniciusdev.com>

---
+++ introdução_JS/primeiro_js/primeior.js
@@ -0,0 +1,32 @@
+let num1 = 15
+num1 = 10
+
+{
+    var a = 15
+    let b = 20
+}
+
+a = 10
+b = 25
+
+var nome = "Luan Muruk"
+var nome2 = 'Luan'
+
+console.log(typeof(nome))
+
+let x = '12'
+let y = Number(x)
+
+console.log(Number.isInteger(y))
+
+x = 12.5984
+console.log(x.toFixed(2))
+
+console.log('15' + 2)
+console.log('aula' * 2)
+console.log(0.9 - 0.5)
+
+const PI = 3.1415
+
+const area = Math.PI * Math.pow(PI, 2)
+console.log(area)
---
+++ introdução_JS/segundo_js/js/script.js
@@ -0,0 +1,3 @@
+//alert("Você entou no cite serto! Aprensa portugueis")
+document.getElementsByTagName('h1')[0].innerHTML = "Ola TagName"
+document.getElementById("primeiro").innerHTML = "Ola ById"
